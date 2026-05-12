b8a96830ca872e588511620f06746365c1824386
rayven <r4ayven@gmail.com>
2026-05-12 18:10:39 +0800

    Bercero - New Password Design

---
+++ DaMockHotel/Form1.Designer.cs
@@ -0,0 +1,257 @@
+﻿namespace DaMockHotel
+{
+    partial class Frm_newPassword
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pnl_Left = new System.Windows.Forms.Panel();
+            this.lbl_Tagline = new System.Windows.Forms.Label();
+            this.lbl_welcomeBack = new System.Windows.Forms.Label();
+            this.lbl_Hotel = new System.Windows.Forms.Label();
+            this.lbl_WelcomeLeft = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pnl_LoginCard = new System.Windows.Forms.Panel();
+            this.lbl_Subtitle = new System.Windows.Forms.Label();
+            this.lbl_Welcome = new System.Windows.Forms.Label();
+            this.txtUsername = new System.Windows.Forms.TextBox();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.lbl_newPassword = new System.Windows.Forms.Label();
+            this.btn_Login = new System.Windows.Forms.Button();
+            this.lbl_Password = new System.Windows.Forms.Label();
+            this.pnl_Left.SuspendLayout();
+            this.pnl_LoginCard.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pnl_Left
+            // 
+            this.pnl_Left.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(31)))), ((int)(((byte)(44)))), ((int)(((byte)(67)))));
+            this.pnl_Left.Controls.Add(this.lbl_Tagline);
+            this.pnl_Left.Controls.Add(this.lbl_welcomeBack);
+            this.pnl_Left.Controls.Add(this.lbl_Hotel);
+            this.pnl_Left.Controls.Add(this.lbl_WelcomeLeft);
+            this.pnl_Left.Controls.Add(this.label1);
+            this.pnl_Left.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(203)))), ((int)(((byte)(161)))), ((int)(((byte)(53)))));
+            this.pnl_Left.Location = new System.Drawing.Point(0, 0);
+            this.pnl_Left.Name = "pnl_Left";
+            this.pnl_Left.Size = new System.Drawing.Size(490, 650);
+            this.pnl_Left.TabIndex = 12;
+            // 
+            // lbl_Tagline
+            // 
+            this.lbl_Tagline.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(31)))), ((int)(((byte)(44)))), ((int)(((byte)(67)))));
+            this.lbl_Tagline.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_Tagline.ForeColor = System.Drawing.Color.White;
+            this.lbl_Tagline.Location = new System.Drawing.Point(65, 222);
+            this.lbl_Tagline.Name = "lbl_Tagline";
+            this.lbl_Tagline.Size = new System.Drawing.Size(350, 30);
+            this.lbl_Tagline.TabIndex = 20;
+            this.lbl_Tagline.Text = "Simple. Comfortable. Secure.";
+            // 
+            // lbl_welcomeBack
+            // 
+            this.lbl_welcomeBack.AutoSize = true;
+            this.lbl_welcomeBack.BackColor = System.Drawing.Color.Transparent;
+            this.lbl_welcomeBack.Font = new System.Drawing.Font("Segoe UI", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_welcomeBack.ForeColor = System.Drawing.Color.White;
+            this.lbl_welcomeBack.Location = new System.Drawing.Point(57, 315);
+            this.lbl_welcomeBack.Name = "lbl_welcomeBack";
+            this.lbl_welcomeBack.Size = new System.Drawing.Size(277, 50);
+            this.lbl_welcomeBack.TabIndex = 18;
+            this.lbl_welcomeBack.Text = "Welcome Back";
+            // 
+            // lbl_Hotel
+            // 
+            this.lbl_Hotel.AutoSize = true;
+            this.lbl_Hotel.BackColor = System.Drawing.Color.Transparent;
+            this.lbl_Hotel.Font = new System.Drawing.Font("Segoe UI", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_Hotel.ForeColor = System.Drawing.Color.White;
+            this.lbl_Hotel.Location = new System.Drawing.Point(62, 155);
+            this.lbl_Hotel.Name = "lbl_Hotel";
+            this.lbl_Hotel.Size = new System.Drawing.Size(272, 50);
+            this.lbl_Hotel.TabIndex = 13;
+            this.lbl_Hotel.Text = "DaMock Hotel";
+            // 
+            // lbl_WelcomeLeft
+            // 
+            this.lbl_WelcomeLeft.Location = new System.Drawing.Point(0, 0);
+            this.lbl_WelcomeLeft.Name = "lbl_WelcomeLeft";
+            this.lbl_WelcomeLeft.Size = new System.Drawing.Size(100, 23);
+            this.lbl_WelcomeLeft.TabIndex = 14;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(203)))), ((int)(((byte)(213)))), ((int)(((byte)(225)))));
+            this.label1.Location = new System.Drawing.Point(68, 375);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(126, 13);
+            this.label1.TabIndex = 17;
+            this.label1.Text = "Enter your new password";
+            // 
+            // pnl_LoginCard
+            // 
+            this.pnl_LoginCard.BackColor = System.Drawing.Color.LightGray;
+            this.pnl_LoginCard.Controls.Add(this.lbl_Subtitle);
+            this.pnl_LoginCard.Controls.Add(this.lbl_Welcome);
+            this.pnl_LoginCard.Controls.Add(this.txtUsername);
+            this.pnl_LoginCard.Controls.Add(this.txtPassword);
+            this.pnl_LoginCard.Controls.Add(this.lbl_newPassword);
+            this.pnl_LoginCard.Controls.Add(this.btn_Login);
+            this.pnl_LoginCard.Controls.Add(this.lbl_Password);
+            this.pnl_LoginCard.Location = new System.Drawing.Point(599, 106);
+            this.pnl_LoginCard.Name = "pnl_LoginCard";
+            this.pnl_LoginCard.Size = new System.Drawing.Size(360, 388);
+            this.pnl_LoginCard.TabIndex = 15;
+            // 
+            // lbl_Subtitle
+            // 
+            this.lbl_Subtitle.AutoSize = true;
+            this.lbl_Subtitle.BackColor = System.Drawing.Color.LightGray;
+            this.lbl_Subtitle.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(100)))), ((int)(((byte)(116)))), ((int)(((byte)(139)))));
+            this.lbl_Subtitle.Location = new System.Drawing.Point(97, 90);
+            this.lbl_Subtitle.Name = "lbl_Subtitle";
+            this.lbl_Subtitle.Size = new System.Drawing.Size(160, 13);
+            this.lbl_Subtitle.TabIndex = 15;
+            this.lbl_Subtitle.Text = "Please enter your new password";
+            // 
+            // lbl_Welcome
+            // 
+            this.lbl_Welcome.AutoSize = true;
+            this.lbl_Welcome.BackColor = System.Drawing.Color.LightGray;
+            this.lbl_Welcome.Font = new System.Drawing.Font("Segoe UI", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_Welcome.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(15)))), ((int)(((byte)(23)))), ((int)(((byte)(42)))));
+            this.lbl_Welcome.Location = new System.Drawing.Point(68, 45);
+            this.lbl_Welcome.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lbl_Welcome.Name = "lbl_Welcome";
+            this.lbl_Welcome.Size = new System.Drawing.Size(238, 45);
+            this.lbl_Welcome.TabIndex = 2;
+            this.lbl_Welcome.Text = "Welcome Back";
+            // 
+            // txtUsername
+            // 
+            this.txtUsername.BackColor = System.Drawing.Color.White;
+            this.txtUsername.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtUsername.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtUsername.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(17)))), ((int)(((byte)(24)))), ((int)(((byte)(39)))));
+            this.txtUsername.Location = new System.Drawing.Point(47, 145);
+            this.txtUsername.Margin = new System.Windows.Forms.Padding(4);
+            this.txtUsername.Name = "txtUsername";
+            this.txtUsername.Size = new System.Drawing.Size(270, 27);
+            this.txtUsername.TabIndex = 0;
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtPassword.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtPassword.Location = new System.Drawing.Point(47, 225);
+            this.txtPassword.Margin = new System.Windows.Forms.Padding(4);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(270, 27);
+            this.txtPassword.TabIndex = 1;
+            this.txtPassword.UseSystemPasswordChar = true;
+            // 
+            // lbl_newPassword
+            // 
+            this.lbl_newPassword.AutoSize = true;
+            this.lbl_newPassword.BackColor = System.Drawing.Color.LightGray;
+            this.lbl_newPassword.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_newPassword.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(17)))), ((int)(((byte)(24)))), ((int)(((byte)(39)))));
+            this.lbl_newPassword.Location = new System.Drawing.Point(47, 120);
+            this.lbl_newPassword.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lbl_newPassword.Name = "lbl_newPassword";
+            this.lbl_newPassword.Size = new System.Drawing.Size(97, 17);
+            this.lbl_newPassword.TabIndex = 4;
+            this.lbl_newPassword.Text = "New Password";
+            // 
+            // btn_Login
+            // 
+            this.btn_Login.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(31)))), ((int)(((byte)(44)))), ((int)(((byte)(67)))));
+            this.btn_Login.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn_Login.FlatAppearance.BorderColor = System.Drawing.Color.White;
+            this.btn_Login.FlatAppearance.BorderSize = 0;
+            this.btn_Login.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(29)))), ((int)(((byte)(78)))), ((int)(((byte)(216)))));
+            this.btn_Login.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_Login.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Login.ForeColor = System.Drawing.Color.White;
+            this.btn_Login.Location = new System.Drawing.Point(47, 295);
+            this.btn_Login.Margin = new System.Windows.Forms.Padding(0);
+            this.btn_Login.Name = "btn_Login";
+            this.btn_Login.Size = new System.Drawing.Size(270, 42);
+            this.btn_Login.TabIndex = 3;
+            this.btn_Login.Text = "Confirm";
+            this.btn_Login.UseVisualStyleBackColor = false;
+            // 
+            // lbl_Password
+            // 
+            this.lbl_Password.AutoSize = true;
+            this.lbl_Password.BackColor = System.Drawing.Color.LightGray;
+            this.lbl_Password.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_Password.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(17)))), ((int)(((byte)(24)))), ((int)(((byte)(39)))));
+            this.lbl_Password.Location = new System.Drawing.Point(47, 200);
+            this.lbl_Password.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lbl_Password.Name = "lbl_Password";
+            this.lbl_Password.Size = new System.Drawing.Size(151, 17);
+            this.lbl_Password.TabIndex = 5;
+            this.lbl_Password.Text = "Confirm New Password";
+            // 
+            // Frm_newPassword
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.ClientSize = new System.Drawing.Size(1084, 611);
+            this.Controls.Add(this.pnl_LoginCard);
+            this.Controls.Add(this.pnl_Left);
+            this.Name = "Frm_newPassword";
+            this.Text = "Form1";
+            this.pnl_Left.ResumeLayout(false);
+            this.pnl_Left.PerformLayout();
+            this.pnl_LoginCard.ResumeLayout(false);
+            this.pnl_LoginCard.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel pnl_Left;
+        private System.Windows.Forms.Label lbl_Tagline;
+        private System.Windows.Forms.Label lbl_welcomeBack;
+        private System.Windows.Forms.Label lbl_Hotel;
+        private System.Windows.Forms.Label lbl_WelcomeLeft;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel pnl_LoginCard;
+        private System.Windows.Forms.Label lbl_Subtitle;
+        private System.Windows.Forms.Label lbl_Welcome;
+        private System.Windows.Forms.TextBox txtUsername;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.Label lbl_newPassword;
+        private System.Windows.Forms.Button btn_Login;
+        private System.Windows.Forms.Label lbl_Password;
+    }
+}
